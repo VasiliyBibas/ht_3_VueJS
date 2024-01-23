9ee0ef75cebfa865890cff27ce5ad104a885b878
«Vasiliy <v_bibas@mail.ru>
2024-01-23 21:08:34 +0300

    added files

---
+++ js/index.js
@@ -0,0 +1,59 @@
+
+const App = {
+	data: () => ({
+		newArticle: [
+			{
+				id: 1,
+				imgArticle: "img/article_photo_1.jpg",
+				typeArticle: "Kitchen",
+				titleArticle: "Let’s Get Solution For Building Construction Work",
+				textArticle: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.<br><br>Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+				dateArticle: "26 December,2022"
+			},
+			{
+				id: 2,
+				imgArticle: "img/article_photo_2.jpg",
+				typeArticle: "Kitchen",
+				titleArticle: "Low Cost Latest Invented Interior Designing Ideas.",
+				textArticle: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.<br><br>Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+				dateArticle: "22 December,2022"
+			},
+			{
+				id: 3,
+				imgArticle: "img/article_photo_3.jpg",
+				typeArticle: "Kitchen",
+				titleArticle: "Best For Any Office & Business Interior Solution",
+				textArticle: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.<br><br>Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+				dateArticle: "25 December,2022"
+			},
+			{
+				id: 4,
+				imgArticle: "img/article_photo_4.jpg",
+				typeArticle: "Bedroom",
+				titleArticle: "Let’s Get Solution For Building Construction Work",
+				textArticle: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.<br><br>Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+				dateArticle: "26 December,2022"
+			},
+			{
+				id: 5,
+				imgArticle: "img/article_photo_5.jpg",
+				typeArticle: "Kitchen",
+				titleArticle: "Low Cost Latest Invented Interior Designing Ideas.",
+				textArticle: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.<br><br>Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+				dateArticle: "26 December,2022"
+			},
+			{
+				id: 6,
+				imgArticle: "img/article_photo_6.jpg",
+				titleArticle: "Best For Any Office & Business Interior Solution",
+				textArticle: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.<br><br>Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+				dateArticle: "26 December,2022"
+			},
+		]
+	}),
+	methods: {
+		
+	},
+
+};
+Vue.createApp(App).mount("#app");
